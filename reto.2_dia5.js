91a8e3bfcb3f85e33b7af500ca2e98d90c43c6d9
indiana <indimar.w1@gmail.com>
2023-12-12 11:35:03 +0100

    mi primer commit

---
+++ reto.2_dia5.js
@@ -0,0 +1,32 @@
+// Copia en dicha carpeta el reto 2 del día anterior (día 5).
+
+let op1 = 5;
+let op2 = 6;
+
+const sum = () => op1 + op2;
+console.log(sum(op1, op2));
+
+const subs = op1 => op1 - op2;
+console.log(subs(op1));
+
+const mult = (op1, op2) => op1 * op2;
+console.log(mult(op1, op2));
+
+const div = (op1, op2) => {
+    if (op2 === 0){
+        return "Error, no se puede dividir por cero";
+    } 
+    return op1/op2;
+    };  
+    
+console.log(div(op1, op2));
+
+// Añade a este archivo de funciones, una nueva función que haga el cuadrado de un numero.
+
+function numberCuadrado(n){
+    return n * n;
+}
+
+let n = 5;
+const resultado = numberCuadrado(n) 
+console.log(resultado);
